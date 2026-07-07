d2dbff01c522135ccce12988a7f82db098c199dc
saicharanreddy5678 <saicharanreddy5678@outlook.com>
2026-07-07 15:43:29 +0530

    Delete TFsampletemplate.tf

--- TFsampletemplate.tf
+++
@@ -1,8 +0,0 @@
-resource "local_file" "sampleTF" {
-  filename = "jing.txt"
-  content = "the aura of jing is very strong"
-}
-resource "random_pet" "sampleT1" {
-   length = 3
-   separator = "-"
-}
